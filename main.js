70796c8bab150972f472353714038f6b48f2b01b
Gianlucci Badiali Minarelli <gianlucci.minarelli@gmail.com>
2017-10-17 20:59:19 -0200

    Update HTML (more semantic), add comments to and reorder MYAPP methods

--- main.js
+++ main.js
@@ -4,79 +4,38 @@ MYAPP = {
     // check for the various File API support.
     if (window.File && window.FileReader && window.FileList && window.Blob) {
       // all the File APIs are supported.
-      fileList.addEventListener("change", () => MYAPP.handleFiles(fileList), false);
+      fileList.addEventListener("change", () => {MYAPP.handleFiles(fileList);}, false);
     } else {
       alert('The File APIs are not fully supported in this browser.');
     }
   },
 
   handleFiles(fileList) {
+    // get the elements inside of which we will output some kind of data.
     const parentElements = {
       numFiles : document.getElementById("numFiles"),
       fileData : document.getElementById("fileData")
     }
+    // clear the parents of any previous data.
     MYAPP.clearOutputs(parentElements);
+    // output the number of input files.
     MYAPP.textOutput({data : fileList.files.length, element : parentElements.numFiles});
+    // output the files data.
     for (const file of fileList.files) {
       MYAPP.fileDataOutput({data : file, parentElement : parentElements.fileData});
     }
   },
 
-  fileDataOutput ({data = "", parentElement = null} = {}) {
-    const {name, size, type} = data, newParentElement = document.createElement("div");
-    parentElement.appendChild(newParentElement);
-    MYAPP.fileMetadataOutput({data : {name, size, type}, parentElement : newParentElement});
-    MYAPP.getFileContent({data, parentElement : newParentElement});
-  },
-
-  fileMetadataOutput ({data = "", parentElement = null} = {}) {
-    const metadataString = JSON.stringify(data);
-    MYAPP.fileOutput({data : metadataString, parentElement});
-  },
-
-  getFileContent ({data = "", parentElement = null} = {}) {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const rawText = event.target.result, textLines = rawText.split(/\r\n|\n/);
-      textLines.map((line) => {
-        MYAPP.fileOutput({data : line, parentElement});
-      });
-    };
-    reader.onerror = (event) => {
-      alert(event.target.error.name);
-    };
-    reader.readAsText(data);
-  },
-
   clearOutputs({fileData, numFiles} = {}) {
+    // TODO write error handling code.
     numFiles.textContent = "";
     while (fileData.firstChild) {
       fileData.removeChild(fileData.firstChild);
     }
   },
 
-  fileOutput ({data = "", parentElement = null} = {}) {
-    const newParentElement = document.createElement("div");
-    parentElement.appendChild(newParentElement);
-    MYAPP.linkTest({data, parentElement : newParentElement});
-  },
-
-  linkTest ({data = "", parentElement = null} = {}) {
-    //TODO improve linkRegex
-    const lineStrings = data.split(/\s/), linkRegex = /^https?:\/\/[^\s]+/;
-    lineStrings.map((lineString) => {
-      if (linkRegex.test(lineString)) {
-        let element = document.createElement("a");
-        parentElement.appendChild(element);
-        element.setAttribute("href", lineString);
-        MYAPP.textOutput({data : lineString, element});
-      } else {
-        MYAPP.textOutput({data : lineString, element : parentElement});
-      }
-    });
-  },
-
   textOutput ({data = "", element = null} = {}) {
+    // TODO write better error handling code.
     if (element) {
       if (element.hasChildNodes) {
         const textNode = document.createTextNode(` ${data}`);
@@ -87,6 +46,64 @@ MYAPP = {
     } else {
       console.log('Error: missing parent element.');
     }
+  },
+
+  fileDataOutput ({data = "", parentElement = null} = {}) {
+    // TODO write error handling code.
+    // extract metadata from the file (name, size and type) and define the new parent, that is,
+    // the element inside of which the data (metadata and body content) of each file will be
+    // displayed.
+    const {name, size, type} = data, newParentElement = document.createElement("article");
+    parentElement.appendChild(newParentElement);
+    // output the files metadata.
+    MYAPP.fileMetadataOutput({data : {name, size, type}, parentElement : newParentElement});
+    MYAPP.getFileContent({data, parentElement : newParentElement});
+  },
+
+  fileMetadataOutput ({data = "", parentElement = null} = {}) {
+    // TODO write error handling code.
+    // TODO update the way metadata (name, size and type) is being displayed.
+    const metadataString = JSON.stringify(data);
+    MYAPP.fileOutput({data : metadataString, parentElement});
+  },
+
+  getFileContent ({data = "", parentElement = null} = {}) {
+    // TODO write better error handling code.
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const rawText = event.target.result, textLines = rawText.split(/\r\n|\n/); //CRLF or LF
+      textLines.map((line) => {
+        MYAPP.fileOutput({data : line, parentElement});
+      });
+    };
+    reader.onerror = (event) => {
+      alert(event.target.error.name);
+    };
+    reader.readAsText(data);
+  },
+
+  fileOutput ({data = "", parentElement = null} = {}) {
+    // TODO write error handling code.
+    const newParentElement = document.createElement("p");
+    parentElement.appendChild(newParentElement);
+    // check for links in the text before the output.
+    MYAPP.linkTest({data, parentElement : newParentElement});
+  },
+
+  linkTest ({data = "", parentElement = null} = {}) {
+    // TODO write better error handling code.
+    // TODO improve linkRegex.
+    const lineStrings = data.split(/\s/), linkRegex = /^https?:\/\/[^\s]+/;
+    lineStrings.map((lineString) => {
+      if (linkRegex.test(lineString)) {
+        const element = document.createElement("a");
+        parentElement.appendChild(element);
+        element.setAttribute("href", lineString);
+        MYAPP.textOutput({data : lineString, element});
+      } else {
+        MYAPP.textOutput({data : lineString, element : parentElement});
+      }
+    });
   }
 }
 
